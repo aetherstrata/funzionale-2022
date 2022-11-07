69cb5c03e6037784c6bfa96ff9e02b8521070e46
Davide Pierotti <d.pierotti@live.it>
2022-11-07 14:16:34 +0100

    Esercizio 1.5

---
+++ Esercizio1.5.ml
@@ -0,0 +1,12 @@
+(* Riscrivere le espressioni seguenti in espressioni equivalenti che non contengano if/then/else: *)
+
+(*
+
+1) if cond1 then true else false -> cond1
+2) if cond1 then false else true -> not cond1
+3) if cond1 then cond2 else false -> cond1 and cond2
+4) if cond1 then cond2 else true -> not cond1 or cond2
+5) if cond1 then true else cond2 -> cond1 or cond2
+6) if cond1 then false else cond2 -> not cond1 and cond2
+
+*)
