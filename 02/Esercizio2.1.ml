9ac419af3dc6ebb2c6194f0fd7393ad19a89802b
Davide Pierotti <d.pierotti@live.it>
2022-11-07 15:00:24 +0100

    Esercizio 2.1

---
+++ 02/Esercizio2.1.ml
@@ -0,0 +1,5 @@
+(* Definire una funzione ultime_cifre: int -> int * int che riporti il valore intero delle due ultime cifre di un int. *)
+
+(* int -> int * int *)
+let ultime_cifre x =
+  let modulo = abs x in (modulo/10 mod 10, modulo mod 10);
